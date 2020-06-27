97dbf61e29957c92c9781119de80687b3e614568
Iago Banov <iagobanov@outlook.com>
2020-06-27 13:49:15 -0300

    Update dict response

--- utils/get_owner_tag.py
+++ utils/get_owner_tag.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, render_template, jsonify
 
-def get_owner_tag(tag_list):
+def get_owner_tag(tag_list, info):
     response = dict()
 
     for tag in tag_list:
         if tag['Key'] == 'Owner':
+            response.update(dict(Info = "{}".format(info)))
             response.update(tag)
-    
-    return jsonify(response)
+            
+    return response
